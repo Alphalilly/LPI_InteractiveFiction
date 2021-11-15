35e9386e79b5678613558cfdcb71b0a3d615f386
Alphalilly <zoozoosamak@gmail.com>
2021-11-15 12:49:04 -0400

    welp, lets get started

---
+++ LPI_InteractiveFiction_1stPlayable/LPI_InteractiveFiction_1stPlayable/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPI_InteractiveFiction_1stPlayable
+{
+    /* Project - C#: Interactive Fiction - First Playable:
+     
+     * Develop a first playable build of an interactive fiction game. *
+
+        + Technical Requirements:
+            - string[] story -- stores an interactive story:
+                Includes title and pages.
+                Each page contains:
+                plot text
+                two story options
+                two destination page numbers
+            - Pages that end the story will have no story options and no destination page numbers.
+            - lets user play through the story.
+            - a mini game engine
+                shows story.
+                reads user input.
+                changes page.
+                ends games.
+            - Encapsulation (make use of methods when appropriate).
+            - Comment your code.
+            - Follow coding naming conventions.
+            - Decouple design from code (do not store design of page output within the story array).
+            - create your own story:
+                title
+                unique
+                creative
+                10 pages or longer
+                multiple endings
+
+        + Extra Mile Suggestions:
+            - color
+            - audio
+            - click sound
+            - music
+            - proper text wrapping
+            - no wall-of-text
+            - slow text output
+            - click to speed up
+            - unique, creative, interesting story
+            - story that challenges limited technical specifications
+            - ascii art
+            - splash screen
+            - inventory
+            - file access (required in FINAL project)
+            - read story from file (required in FINAL project)
+            - save game (required in FINAL project)
+            - main menu (required in FINAL project)
+     */
+
+    class Program
+    {
+        static void Main()
+        {
+
+
+        }
+    }
+}
