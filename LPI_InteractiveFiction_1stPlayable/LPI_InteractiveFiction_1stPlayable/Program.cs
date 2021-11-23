5517ba2849777da0b54c7336754ee24feb18d12c
Alphalilly <zoozoosamak@gmail.com>
2021-11-23 17:48:27 -0400

    ok. Binary Trees exist. I want to try that here
    I wanna see if I can use Binary Trees for the pages in the array. (or some basic version of it). Was talking to a friend about how to approach the assignment and gave me this idea.
    It might be way too advanced for me but I think it would be something worth trying and learning.

--- LPI_InteractiveFiction_1stPlayable/LPI_InteractiveFiction_1stPlayable/Program.cs
+++ LPI_InteractiveFiction_1stPlayable/LPI_InteractiveFiction_1stPlayable/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions; //Regex https://regex101.com/ should I use this??
 
 namespace LPI_InteractiveFiction_1stPlayable
 {
@@ -60,13 +59,187 @@ namespace LPI_InteractiveFiction_1stPlayable
     {
         static void Main()
         {
-            //Story.anothertest();
+            Story.ParseStory(Story.story);
+
+            Console.ReadKey(true);
+
         }
+    }
+
+    public class PageNode
+    {
+        public string plotText;
+        public string choiceTextA;
+        public string choiceTextB;
+
+        public PageNode choiceA;
+        public PageNode choiceB;
     }
 
     public class Story
     {
-        static string[,] story = new string[,]
+        //using this old story as a placeholder
+        public static string[] story = new string[]
+        {
+                //Title Page | page:0
+                "NSCC Game Development - Logic and Programming 1 " +
+                ";An interesting Dinner " +
+                ";-----------------.",
+                //-----------------------------------------------------------------------------------------------
+                //1st | page:1
+                " *Your trying to decide to eat Stake or Sushi tonight, both seem appealing but you just cant decide!" +
+                ";I'll go with the stake, its not expensive and its good." +
+                ";Sushi time! I've been craving it for a while and I deserve to treat myself every once in a while." +
+                ";2" +
+                ";3", 
+                //A: stake | page:2
+                "Eh I Think I'll go with the Stake, basic and good. Though Should I eat out? or buy what I need and make it myself?" +
+                ";I'll eat out. I was thinking about trying stake from this new place anyway." +
+                ";I think I'll make the Stake, kinda wanted to try out this new recipe for a while." +
+                ";4" +
+                ";5", 
+                //-----------------------------------------------------------------------------------------------
+                //B: sushi | page:3
+                "I'll choose Sushi! I'm feeling experimental today. There's a nearby sushi place that's pretty close, or should I ask an old friend I know and try out his Sushi?" +
+                ";I'll go to that Sushi place, The sweet old lady there always likes to give me extra mints before I leave." +
+                ";Eh. I'll give an old freind a visit. Maybe catch up and finally try his Sushi recipe." +
+                ";6" +
+                ";7", 
+                //-----------------------------------------------------------------------------------------------
+                // stake / A: out | page:4
+                "I'm gonna eat out, too lazy to cook it myself. I was thinking about hanging out with the boys, haven't talked to them much lately. But I'm not sure." +
+                ";Eh, they can be a bit noisy, I might just head to the stake house on my own." +
+                ";Aaah Ill invite em all, we could have a Stake party! " +
+                ";8" +
+                ";9", 
+                // stake / B: cook | page:5
+                "It wouldn't hurt to try my hand at cooking! I just gotta get the ingredients. should I eat alone, or invite my friends over?" +
+                ";Nah, I think I'm gonna eat alone, kinda feel like having time to myself." +
+                ";yeah I dont mind having some company, and crack open a cold one with the boys after lunch." +
+                ";10" +
+                ";11",
+                //-----------------------------------------------------------------------------------------------
+                // sushi / A: out | page:6
+                "The Sushi place sounds like a good idea, been a while since I last been there. Now that I think about it, I could go alone, or I could invite a few friends." +
+                ";Nah I'll go alone, saves me some money anyway." +
+                ";Yeah I'll hang out with the boys, See what they've been up to lately." +
+                ";12" +
+                ";13",
+                // sushi / B: oldfriend | page:7
+                "I Think I'll visit my old friend, see if he's had any luck getting into Psychology School or whatever he calls it. We could catch up on old times while he makes Sushi, or I could invite some friends to finally meet him." +
+                ";I dont want to bother him with a big crowed, It'll just be me and him, with some delicious sushi" +
+                ";Eh, ill bring a few friends along, I'm sure he wont mind." +
+                ";14" +
+                ";15",
+                //-----------------------------------------------------------------------------------------------
+                // stake / out / A: alone | page:8
+                " *You decided to head to the Stake House alone." +
+                ";" +
+                ";" +
+                ";" +
+                ";",
+                // stake / out / B: friends | page:9
+                " *You decided to head to the Stake House with da bois." +
+                ";" +
+                ";" +
+                ";" +
+                ";",
+                // stake / cook / A: alone | page:10
+                " *You decided to cook stake and have lunch alone." +
+                ";" +
+                ";" +
+                ";" +
+                ";",
+                // stake / cook / B: friends | page:11
+                " *You decided to cook stake and party with da bois." +
+                ";" +
+                ";" +
+                ";" +
+                ";",
+                //-----------------------------------------------------------------------------------------------
+                // sushi / out / A: alone | page:12
+                " *You decided to head to the Sushi restaurant alone." +
+                ";" +
+                ";" +
+                ";" +
+                ";",
+                // sushi / out / B: buddies | page:13
+                " *You decided to head to the Sushi restaurant with da bois." +
+                ";" +
+                ";" +
+                ";" +
+                ";",
+                // sushi / oldfriend / A: alone | page:14
+                " *You decided to visit your old friend for Sushi alone." +
+                ";" +
+                ";" +
+                ";" +
+                ";",
+                // sushi / oldfriend / B: buddies | page:15
+                " *You decided to visit your old friend for Sushi with da bois." +
+                ";" +
+                ";" +
+                ";" +
+                ";",
+        };
+
+        static bool IsActive = true;
+
+        public static PageNode ParseStory(string[] storyData)
+        {
+            Dictionary<int, PageNode> pages = new Dictionary<int, PageNode>();
+
+            for (int i = 0; i < storyData.Length; i++)
+            {
+                string pageSource = storyData[i];
+                PageNode pageNode = new PageNode();
+                string[] storySplit = pageSource.Split(';');
+
+                pageNode.plotText = storySplit[0];
+                pageNode.choiceTextA = storySplit[1];
+                pageNode.choiceTextB = storySplit[2];
+
+                pages.Add(i, pageNode);
+            }
+
+            for (int j = 0; j < storyData.Length; j++)
+            {
+                string pageSource = storyData[j];
+                string[] storySplit = pageSource.Split(';');
+                PageNode pageNode = pages[j];
+
+                pageNode.choiceA = pages[int.Parse(storySplit[3])];
+                pageNode.choiceB = pages[int.Parse(storySplit[4])];
+            }
+
+            return pages[0];
+        }
+
+        //public static void printstory()
+        //{
+
+
+
+        //    for (int i = 0; i < story.Length; i++)
+        //    {
+
+
+        //        Console.WriteLine(story[i]);
+        //        Console.ReadKey(true);
+        //    }
+
+        //    //while (IsActive == true)
+        //    //{
+
+        //    //}
+        //}
+    }
+
+    /* i wanna die
+    using System.Text.RegularExpressions; //Regex https://regex101.com/ should I use this??
+    public class StoryB
+    {
+        static string[,] nostory = new string[,]
             {
                 //The "Idk what im doing" Diagram:
 
@@ -109,7 +282,7 @@ namespace LPI_InteractiveFiction_1stPlayable
 
         public static void anothertest(int pageNum, int paragraphNum) // y, x
         {
-            List<string> sortedStory = new List<string>(story[pageNum, paragraphNum].Split(';')); //wait... this works?!
+            List<string> sortedStory = new List<string>(nostory[pageNum, paragraphNum].Split(';')); //wait... this works?!
 
             foreach (string paragraph in sortedStory)
             {
@@ -273,29 +446,30 @@ namespace LPI_InteractiveFiction_1stPlayable
         }
 
         // idea for a menu
-        /* //Main Menu
-         * "Title"
-         * 
-         * > New Game
-         *      "select a new story"
-         *      > story1
-         *      > story2
-         *      > story3
-         *      > + Add story from file
-         *      > < back
-         *      
-         * > Load
-         *      "Select a prevous save"
-         *      > story1
-         *      > story2
-         *      > story3
-         *      > - Delete a Save
-         *      > < back
-         *      
-         * > Quit
-         *      "Are you sure"
-         *      > Y/N
-         * 
-         */
+         //* //Main Menu
+         //* "Title"
+         //* 
+         //* > New Game
+         //*      "select a new story"
+         //*      > story1
+         //*      > story2
+         //*      > story3
+         //*      > + Add story from file
+         //*      > < back
+         //*      
+         //* > Load
+         //*      "Select a prevous save"
+         //*      > story1
+         //*      > story2
+         //*      > story3
+         //*      > - Delete a Save
+         //*      > < back
+         //*      
+         //* > Quit
+         //*      "Are you sure"
+         //*      > Y/N
+         //* 
+         
     }
+    */
 }
